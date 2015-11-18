d11e0711f73ca39d9500d286e7a817561d868c7a
Maddy <maddy531@gmail.com>
2015-11-18 16:26:06 -0500

    Added phrases

--- script.js
+++ script.js
@@ -21,17 +21,24 @@ angular.module("myMod",[])
       'spin '
     ];
 
+    $scope.randomPhrases=[
+      'Jump on the bandwagon',
+      'A miss is as good as a mile',
+      'On your beam ends',
+      'Thats all she wrote',
+      'Dont shoot the messenger'
+    ];
 
 
     $scope.wordsOnPage = [];
 
     $scope.talk = function(){
-      var mixFuncs = [addDecorWord, addUndecorWord];
+      var mixFuncs = [addDecorWord, addUndecorWord, addPhrases];
       function rando (){
-         return mixFuncs[Math.floor(Math.random()*mixFuncs.length)];
+         return mixFuncs[Math.floor(Math.random()*mixFuncs.length)]();
        };
 
-        $interval(rando(), 500, 10);
+        $interval(rando, 500, 10);
     }
 
      function addUndecorWord (){
@@ -47,6 +54,13 @@ angular.module("myMod",[])
       $scope.wordsOnPage.push(newElement);
     }
 
-    $interval
+    function addPhrases(){
+     var newElement = {};
+     newElement.word =  $scope.randomWords[Math.floor(Math.random()* $scope.randomWords.length)];
+     newElement.decor = $scope.randomClass[Math.floor(Math.random()* $scope.randomClass.length)];
+     newElement.phrase =  $scope.randomPhrases[Math.floor(Math.random()* $scope.randomPhrases.length)];
+     $scope.wordsOnPage.push(newElement);
+   }
+
 
   });
