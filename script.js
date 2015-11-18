13f8dd2caef3d01d32741304837ab68e619753cf
Maddy <maddy531@gmail.com>
2015-11-18 16:33:59 -0500

    Refactored JS

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ angular.module("myMod",[])
     'Hello ',
     'Random ',
     'Words ',
-    'Oops '
+    'Oops ',
+    'Jump on the bandwagon',
+    'A miss is as good as a mile',
+    'On your beam ends',
+    'Thats all she wrote',
+    'Dont shoot the messenger'
     ];
 
     $scope.randomClass=[
@@ -21,19 +26,10 @@ angular.module("myMod",[])
       'spin '
     ];
 
-    $scope.randomPhrases=[
-      'Jump on the bandwagon',
-      'A miss is as good as a mile',
-      'On your beam ends',
-      'Thats all she wrote',
-      'Dont shoot the messenger'
-    ];
-
-
     $scope.wordsOnPage = [];
 
     $scope.talk = function(){
-      var mixFuncs = [addDecorWord, addUndecorWord, addPhrases];
+      var mixFuncs = [addDecorWord];
       function rando (){
          return mixFuncs[Math.floor(Math.random()*mixFuncs.length)]();
        };
@@ -41,26 +37,13 @@ angular.module("myMod",[])
         $interval(rando, 500, 10);
     }
 
-     function addUndecorWord (){
-      var newElement = {};
-      newElement.word =  $scope.randomWords[Math.floor(Math.random()* $scope.randomWords.length)];
-      newElement.decor = '';
-      $scope.wordsOnPage.push(newElement);
-    }
+
      function addDecorWord(){
       var newElement = {};
       newElement.word =  $scope.randomWords[Math.floor(Math.random()* $scope.randomWords.length)];
       newElement.decor = $scope.randomClass[Math.floor(Math.random()* $scope.randomClass.length)];
       $scope.wordsOnPage.push(newElement);
     }
-
-    function addPhrases(){
-     var newElement = {};
-     newElement.word =  $scope.randomWords[Math.floor(Math.random()* $scope.randomWords.length)];
-     newElement.decor = $scope.randomClass[Math.floor(Math.random()* $scope.randomClass.length)];
-     newElement.phrase =  $scope.randomPhrases[Math.floor(Math.random()* $scope.randomPhrases.length)];
-     $scope.wordsOnPage.push(newElement);
-   }
 
 
   });
